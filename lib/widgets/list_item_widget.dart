eef784f7282dfad47cf1ed21be77f3f0651cf9ff
schmeekygeek <ss6087982@gmail.com>
2022-10-01 01:33:08 +0530

    finish animating checkbox halfway through

--- lib/widgets/list_item_widget.dart
+++ lib/widgets/list_item_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:lottie/lottie.dart';
 import 'package:provider/provider.dart';
 import 'package:todo/classes/task.dart';
 import 'package:todo/providers/task_model.dart';
@@ -16,6 +17,26 @@ class ListItem extends StatefulWidget {
 }
 
 class _ListItemState extends State<ListItem> with TickerProviderStateMixin {
+
+  late final AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      value: 2,
+      vsync: this,
+      duration: const Duration(seconds: 1),
+      reverseDuration: const Duration(milliseconds: 200),
+    );
+    _controller.reverse();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return AnimatedOpacity(
@@ -42,21 +63,47 @@ class _ListItemState extends State<ListItem> with TickerProviderStateMixin {
             ),
           ),
         ),
-        child: CheckboxListTile(
-          checkboxShape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(5))
-          ),
-          value: widget.task.getIsDone,
-          onChanged: (value) => context.read<TaskModel>().toggleDone(widget.task),
-          title: Text(
-            widget.task.getText!,
-            style: TextStyle(
-              decoration: (widget.task.getIsDone) ? TextDecoration.lineThrough : TextDecoration.none,
-              color: context.watch<ThemeModel>().isDark ? Colors.white : Colors.black87,
-              fontFamily: "Poppins",
-              fontSize: 15,
-              fontWeight: FontWeight.w100
-            ),
+        // https://assets8.lottiefiles.com/temp/lf20_eDCMYv.json
+        child: ListTile(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                flex: 8,
+                child: Text(
+                  widget.task.getText!,
+                  style: TextStyle(
+                    overflow: TextOverflow.clip,
+                    decoration: (widget.task.getIsDone) ? TextDecoration.lineThrough : TextDecoration.none,
+                    color: context.watch<ThemeModel>().isDark ? Colors.white : Colors.black87,
+                    fontFamily: "Poppins",
+                    fontSize: 15,
+                    fontWeight: FontWeight.w100
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: GestureDetector(
+                  onTap: () {
+                    context.read<TaskModel>().toggleDone(widget.task);
+                    if(widget.task.isDone){
+                      _controller.forward();
+                    }
+                    else {
+                      _controller.reverse();
+                    }
+                  },
+                  child: Lottie.network(
+                    "https://assets8.lottiefiles.com/temp/lf20_eDCMYv.json",
+                    // "https://assets6.lottiefiles.com/packages/lf20_1wu8wbcn.json",
+                    controller: _controller,
+                    width: 80,
+                    height: 80,
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
